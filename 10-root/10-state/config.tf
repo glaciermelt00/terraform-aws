e1bca2fb965b136e72fdc40bd29944270ef0b848
Jo Harada <glacier-melt@hotmail.co.jp>
2022-09-18 00:11:59 +0900

    feat: modify bucket name

--- 10-root/10-state/config.tf
+++ 10-root/10-state/config.tf
@@ -4,7 +4,7 @@ terraform {
   # State Storage and Locking
   backend s3 {
     region         = "ap-northeast-1"
-    bucket         = "terraform-308890121156.secondgame.net"
+    bucket         = "terraform-814937260541.glaciermelt00"
     key            = "state"
     dynamodb_table = "terraform_lock"
     shared_credentials_file = "$HOME/.aws/credentials"
