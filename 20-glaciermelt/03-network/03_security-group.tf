29018e313053315b1f2831c7545ab1fd55f43859
Jo Harada <glacier-melt@hotmail.co.jp>
2022-09-18 22:21:08 +0900

    feat: delete unused security groups

--- 20-glaciermelt/03-network/03_security-group.tf
+++ 20-glaciermelt/03-network/03_security-group.tf
@@ -27,30 +27,3 @@ resource aws_security_group_rule default-2 {
   to_port           = 0
   type              = "egress"
 }
-
-/**
- * For load balancer
- */
-resource aws_security_group www {
-  vpc_id      = aws_vpc.default.id
-  name        = "www"
-  description = "Network load balancer"
-}
-
-resource aws_security_group_rule www-1 {
-  security_group_id = aws_security_group.www.id
-  type              = "ingress"
-  from_port         = 80
-  to_port           = 80
-  protocol          = "tcp"
-  cidr_blocks       = [ "0.0.0.0/0" ]
-}
-
-resource aws_security_group_rule www-2 {
-  security_group_id = aws_security_group.www.id
-  type              = "ingress"
-  from_port         = 443
-  to_port           = 443
-  protocol          = "tcp"
-  cidr_blocks       = [ "0.0.0.0/0" ]
-}
